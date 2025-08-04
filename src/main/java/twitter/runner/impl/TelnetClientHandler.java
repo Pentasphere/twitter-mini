bd112927d3fd95cf8af916e8e11b2388982e3977
Pentasphere <127765465+Pentasphere@users.noreply.github.com>
2025-08-04 16:52:59 +0600

    MT-1: Fixed the handling of TwitterCommandException.class (#1)

--- src/main/java/twitter/runner/impl/TelnetClientHandler.java
+++ src/main/java/twitter/runner/impl/TelnetClientHandler.java
@@ -1,6 +1,7 @@
 package twitter.runner.impl;
 
 import twitter.exception.ClientDisconnectedException;
+import twitter.exception.TwitterCommandException;
 import twitter.exception.UnknownCommandException;
 import twitter.factory.CommandFactory;
 import twitter.factory.CommandFactoryBuilder;
@@ -38,9 +39,12 @@ public class TelnetClientHandler implements Runnable {
                     writer.flush();
                     command = reader.readLine();
                     commandFactory.getHandler(command).handle();
+                } catch (TwitterCommandException ex){
+                    writer.write(ex.getMessage() + "\n");
+                    System.out.println(ex.getLocalizedMessage());
                 } catch (UnknownCommandException ex) {
                     try {
-                        writer.write("Команда неопознана, проверьте список команд и попробуйте снова.");
+                        writer.write("Команда неопознана, проверьте список команд и попробуйте снова.\n");
                     } catch (IOException ex1) {
                         System.out.println(ex.getMessage());
                     }
