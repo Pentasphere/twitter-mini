55269a0de0cecef244dfd65a0d254c04bf5e48bd
Pentasphere <tilya.pentathlon@gmail.com>
2025-08-19 15:31:22 +0600

    Added password encoder

--- src/main/java/twitter/service/impl/UserServiceImpl.java
+++ src/main/java/twitter/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package twitter.service.impl;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import twitter.configuration.Component;
 import twitter.configuration.Injection;
 import twitter.dao.UserDAO;
@@ -27,11 +28,13 @@ public class UserServiceImpl implements UserService {
     }*/
 
     private final UserDAO userDAO;
+    private final PasswordEncoder passwordEncoder;
 
     @Injection
-    public UserServiceImpl(UserDAO userDAO) {
+    public UserServiceImpl(UserDAO userDAO, PasswordEncoder passwordEncoder) {
 
         this.userDAO = userDAO;
+        this.passwordEncoder = passwordEncoder;
     }
 
     /*private UserServiceImpl(UserDAO userDAO) {
@@ -54,6 +57,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User createUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userDAO.saveNewUser(user);
     }
 
